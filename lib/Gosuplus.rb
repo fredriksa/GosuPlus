76ffeb1c089cf145406e071d23287a045e30d0f6
Freddan962 <fredde.sander@gmail.se>
2015-02-15 20:14:36 +0100

    New version

--- lib/Gosuplus.rb
+++ lib/Gosuplus.rb
@@ -1,2 +1,12 @@
 require 'gosu'
-Dir['./Gosuplus/.*rb'].each {|file| require file}
+
+Dir['/Gosuplus/.*rb'].each {|file| require file}
+Dir['/Gosuplus/states/.*rb'].each {|file| require file}
+Dir['/Gosuplus/gui/.*rb'].each {|file| require file}
+Dir['/Gosuplus/gui/form/.*rb'].each {|file| require file}
+
+module Gosuplus
+  def self.test
+    puts "TEST"
+  end
+end
