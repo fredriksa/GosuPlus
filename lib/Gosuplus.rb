036997c8a029077a31add89ce96d08708d377b16
Freddan962 <fredde.sander@gmail.se>
2015-02-15 20:37:59 +0100

    New version

--- lib/Gosuplus.rb
+++ lib/Gosuplus.rb
@@ -1,9 +1,37 @@
 require 'gosu'
 
-Dir['./Gosuplus/.*rb'].each {|file| require file}
-Dir['./Gosuplus/states/.*rb'].each {|file| require file}
-Dir['./Gosuplus/gui/.*rb'].each {|file| require file}
-Dir['./Gosuplus/gui/form/.*rb'].each {|file| require file}
+require_relative 'Gosuplus/body'
+require_relative 'Gosuplus/collision'
+require_relative 'Gosuplus/contentmanager'
+require_relative 'Gosuplus/drawable'
+require_relative 'Gosuplus/drawable_rot'
+require_relative 'Gosuplus/entity'
+require_relative 'Gosuplus/essentials'
+require_relative 'Gosuplus/resourceloader'
+require_relative 'Gosuplus/resourcemanager'
+require_relative 'Gosuplus/state'
+require_relative 'Gosuplus/statemanager'
+require_relative 'Gosuplus/staterenderer'
+require_relative 'Gosuplus/stateupdater'
+require_relative 'Gosuplus/timer'
+require_relative 'Gosuplus/vec2'
+require_relative 'Gosuplus/vec3'
+require_relative 'Gosuplus/version'
+require_relative 'Gosuplus/window'
+
+require_relative 'Gosuplus/gui/button'
+require_relative 'Gosuplus/gui/buttonrenderer'
+require_relative 'Gosuplus/gui/gui'
+require_relative 'Gosuplus/gui/guirenderer'
+require_relative 'Gosuplus/gui/icon'
+require_relative 'Gosuplus/gui/text'
+require_relative 'Gosuplus/gui/toggleable'
+
+require_relative 'Gosuplus/gui/form/base'
+require_relative 'Gosuplus/gui/form/renderer'
+require_relative 'Gosuplus/gui/form/text'
+
+require_relative 'Gosuplus/states/example_state'
 
 module Gosuplus
   def self.test
