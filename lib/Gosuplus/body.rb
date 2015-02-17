ec3cfa38476051d600a45c0412f1ad4ec1edea7e
Freddan962 <fredde.sander@gmail.se>
2015-02-17 20:33:55 +0100

    angle -> a

--- lib/Gosuplus/body.rb
+++ lib/Gosuplus/body.rb
@@ -3,7 +3,7 @@ module Gosuplus
     attr_accessor :position, :a, :width, :height
     
     def initialize(position, width, height, angle = 90)
-      @position, @width, @height, @angle = position, width, height, angle
+      @position, @width, @height, @a = position, width, height, angle
     end
   end
 end
