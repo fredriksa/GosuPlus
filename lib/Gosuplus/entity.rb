841122e68a1d4de5217868b3de331694f31e7054
Freddan962 <fredde.sander@gmail.se>
2015-02-17 19:38:48 +0100

    Modified base functionality

--- lib/Gosuplus/entity.rb
+++ lib/Gosuplus/entity.rb
@@ -1,5 +1,11 @@
 module Gosuplus
-  module Entity
+  class Entity
     attr_accessor :id
+    include DrawableRot
+
+    def initialize(pos, image)
+      @body = Body.new(pos, image.width, image.height)
+      @image = image
+    end
   end
 end
